96f24184526557fd4b33928a3b66163177d457b8
rwolf-eng <57576835+rwolf-eng@users.noreply.github.com>
2020-08-09 22:19:24 +0200

    Update deneme.java
    github ta degisikliklik yaptim

--- src/git_project_ikinci/deneme.java
+++ src/git_project_ikinci/deneme.java
@@ -4,6 +4,9 @@ public class deneme {
 
 	public static void main(String[] args) {
 		System.out.println("ikinci gir projem");
+		System.out.println("bunu yanlis  yazmisim duzeltiyorum.");
+		System.out.println("remout degsiklik");
+		
 	}
 
 }
