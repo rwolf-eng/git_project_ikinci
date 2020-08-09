b26a4ecde41bc0557992c443c02c7709c0eae93a
rwolf-eng <remziwolf@gmail.com>
2020-08-09 22:07:34 +0200

    ikinci projemin ilk kommiti

--- src/git_project_ikinci/deneme.java
+++ src/git_project_ikinci/deneme.java
@@ -3,8 +3,7 @@ package git_project_ikinci;
 public class deneme {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		System.out.println("ikinci gir projem");
 	}
 
 }
